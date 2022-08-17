30720de590f1f2afa4016b5c1cd62971fa140dfa
luanrem <luanrem@gmail.com>
2022-08-17 19:49:22 -0300

    initial projects

---
+++ lesson4-Webpack/04-webpack/src/script.js
@@ -0,0 +1,38 @@
+import "./style.css";
+import * as THREE from "three";
+
+console.log(THREE);
+
+// Scene
+const scene = new THREE.Scene();
+
+// Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+scene.add(camera);
+
+camera.position.z = 3;
+camera.position.y = 1;
+camera.position.x = 1;
+
+// Get Canvas from DOM
+const canvas = document.querySelector(".webgl");
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
